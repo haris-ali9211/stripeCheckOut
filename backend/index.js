699a29396b0cc770a1c807de4c79d85fdff3b55b
Haris Ali <64413337+haris-ali9211@users.noreply.github.com>
2023-03-07 22:14:34 +0500

    added backend

--- backend/index.js
+++ backend/index.js
@@ -3,4 +3,28 @@ const app = express()
 const port = 5000;
 const dotenv = require("dotenv");
 const mongoose = require('mongoose');
+const stripe = require("./routes/stripe")
+var cors = require('cors');
 
+app.use(cors())
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log("Connection successful");
+}).catch((err) => {
+    console.log("Connection not successful", err);
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.use('/api/checkout/', stripe);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ backend/routes/stripe.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const Stripe = require("stripe")
+const router =express.Router();
+
+require("dotenv").config();
+
+const stripe = Stripe(process.env.STRIPE_KEY)
+
+
+router.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${process.env.URL_CLIENT}success,`,
+    cancel_url:  `${process.env.URL_CLIENT}cancel,`,
+  });
+
+  res.send({
+    url: session.url
+  });
+});
+
+module.exports = router;
